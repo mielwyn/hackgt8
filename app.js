dbddad32e9e9387e266d9301659bba463642f412
Micah <mielwyn@gmail.com>
2021-10-24 00:07:05 -0400

    put static resources in public folder

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const port = 3000
 const MongoClient = require('mongodb').MongoClient
 const passwrd = require('./config/keys').atlasPassword
 const connectionString = 'mongodb+srv://micah:'+passwrd+'@cluster0.45jzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const path = require('path')
+app.use(express.static(__dirname+'/public'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -19,6 +21,9 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 		app.post('/', (req, res) => {
 			res.send('Hello World!')
 			fittenCollection.insertOne({'num_unique':req.body.num_unique,'time':Date.now()})
+		})
+		app.get('/',(req,res)=>{
+			res.sendFile(path.join(__dirname,'BuzzSense.html'))
 		})
 		app.listen(port, () => {
 			console.log('Started listening at http://localhost:'+port)
