ee2eb3be70849aa1cc7d8a28189e3675ae4afb4b
Micah <mielwyn@gmail.com>
2021-10-23 23:40:12 -0400

    database shit set up lol

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser')
+const port = 3000
+const MongoClient = require('mongodb').MongoClient
+const passwrd = require('./config/keys').atlasPassword
+const connectionString = 'mongodb+srv://micah:'+passwrd+'@cluster0.45jzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+	.then(client => {
+		console.log('Connected to Database')
+		const db = client.db('building-occupancy')
+		const fittenCollection = db.collection('fitten')
+		app.post('/', (req, res) => {
+			res.send('Hello World!')
+			fittenCollection.insertOne({'num_unique':req.body.num_unique,'time':Date.now()})
+		})
+		app.listen(port, () => {
+			console.log('Started listening at http://localhost:'+port)
+		})
+	  })
+  .catch(error => console.error(error))
--- rpi-monitor/monitor.py
+++ rpi-monitor/monitor.py
@@ -1,31 +1,33 @@
 import os
+import requests
 from scapy.all import *
-os.system('sh s.sh')
+import json
 
-# rdpcap comes from scapy and loads in our pcap file
-packets = rdpcap('testfile.pcap')
+while True:
+	os.system('sh s.sh')
 
-ipdict = {}
-counter = 0
-for packet in packets:
-	if IP in packet:
-		if ipdict.has_key(packet[IP].src) and ipdict.has_key(packet[IP].dst):
-			ipdict[packet[IP].src] = ipdict[packet[IP].src] + 1
-			ipdict[packet[IP].dst] = ipdict[packet[IP].dst] + 1
-		elif ipdict.has_key(packet[IP].src):
-			ipdict[packet[IP].src] = ipdict[packet[IP].src] + 1
-			ipdict[packet[IP].dst] = 1
-			print('unique ip: ',packet[IP].dst)
-			counter += 1
-		elif ipdict.has_key(packet[IP].dst):
-			ipdict[packet[IP].dst] = ipdict[packet[IP].dst] + 1
-			ipdict[packet[IP].src] = 1
-			print('unique ip: ',packet[IP].src)
-			counter += 1
-		else:
-			ipdict[packet[IP].src] = 1
-			ipdict[packet[IP].dst] = 1
-			print('unique ip: ',packet[IP].src)
-			print('unique ip: ',packet[IP].dst)
-			counter += 2
-print(str(counter) + '/'+ str(len(packets)*2) + ' unique IPs')
+	# rdpcap comes from scapy and loads in our pcap file
+	packets = rdpcap('testfile.pcap')
+	server_ip = 'http://128.61.26.2:3000'
+	ipdict = {}
+	counter = 0
+	for packet in packets:
+		if IP in packet:
+			if ipdict.has_key(packet[IP].src) and ipdict.has_key(packet[IP].dst):
+				ipdict[packet[IP].src] = ipdict[packet[IP].src] + 1
+				ipdict[packet[IP].dst] = ipdict[packet[IP].dst] + 1
+			elif ipdict.has_key(packet[IP].src):
+				ipdict[packet[IP].src] = ipdict[packet[IP].src] + 1
+				ipdict[packet[IP].dst] = 1
+				counter += 1
+			elif ipdict.has_key(packet[IP].dst):
+				ipdict[packet[IP].dst] = ipdict[packet[IP].dst] + 1
+				ipdict[packet[IP].src] = 1
+				counter += 1
+			else:
+				ipdict[packet[IP].src] = 1
+				ipdict[packet[IP].dst] = 1
+				counter += 2
+	toSend = {'name':'chipi','num_unique':counter}
+	r = requests.post(server_ip,data=toSend)
+	time.sleep(600)
