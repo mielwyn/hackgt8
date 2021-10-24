de98c8a9221180f22dbc537f91c163de829a829d
Micah <mielwyn@gmail.com>
2021-10-24 01:08:05 -0400

    functional most recent data endpoint

--- app.js
+++ app.js
@@ -14,14 +14,25 @@ app.use(bodyParser.json())
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
 	.then(client => {
+
 		console.log('Connected to Database')
 		const db = client.db('building-occupancy')
 		app.post('/submitdata', (req, res) => {
 			db.collection(req.body.name).insertOne({'num_unique':parseInt(req.body.num_unique),'time':Date.now()})
 			console.log('poggers database moment')
 		})
-		app.get('/getdata',(req,res)=>{
-			res.send(db.listCollections().toArray())
+		//http://localhost:3000/mostrecentdata?zone=chiroom
+		app.get('/mostrecentdata',(req,res)=>{
+			/*db.listCollections().toArray((err, collections) => {
+				res.send(collections);
+			});*/
+			if (req.query.zone == null){
+				res.send('specify which zone you want data for with a query')
+			}
+			else{
+				db.collection(req.query.zone).find({}).sort({time:1}).limit(1).toArray((err,result)=>{
+				res.send(result);
+			})}
 		})
 		app.get('/',(req,res)=>{
 			res.sendFile(path.join(__dirname,'BuzzSense.html'))
